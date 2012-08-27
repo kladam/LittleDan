878ca9c9d7c5ab035cc199c38f5cfb0f4aa45c8b
Kieran <kieranlad@gmail.com>
2012-08-27 17:18:07 +0100

    Changed main to Utility

---
+++ js/Utility.js
@@ -0,0 +1,26 @@
+var Utility = (function(){
+
+    function Utility()
+    {
+        this.imgLib = {};
+    }
+
+    Utility.prototype.loadImage = function(name, url){
+        var img = new Image();
+        img.src = url;
+        img.setAttribute('name', name);
+
+        img.onload = function(){
+            // dispatch event image loaded (name)
+        };
+
+        this.imgLib[name] = img;
+    };
+
+    Utility.prototype.loadSound = function(){
+        // to be implemented
+    };
+
+    return Utility;
+
+}());
