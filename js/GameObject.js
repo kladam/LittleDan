8b922fae7e576a6de445e19dc9cebe731ed92348
Adam <adamwhiteland@googlemail.com>
2012-08-27 18:09:45 +0100

    added vars to GameObject prototype

--- js/GameObject.js
+++ js/GameObject.js
@@ -2,12 +2,12 @@ var GameObject = (function()
 {
 	function GameObject()
 	{
-		this.x = 0;
-		this.y = 0;
-		this.w = 0;
-		this.h = 0;
-		this.xs = 0;
-		this.ys = 0;
+		GameObject.prototype.x = 0;
+		GameObject.prototype.y = 0;
+		GameObject.prototype.w = 0;
+		GameObject.prototype.h = 0;
+		GameObject.prototype.xs = 0;
+		GameObject.prototype.ys = 0;
 	}
 
 	GameObject.prototype.draw = function(){};
